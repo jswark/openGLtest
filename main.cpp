f208e012800b6afb12b7adb55197a4a4b45a3294
jswark <swark.a.j@gmail.com>
2020-11-02 20:55:59 +0300

    learnopengl 1.3

---
+++ main.cpp
@@ -0,0 +1,53 @@
+// GLEW нужно подключать до GLFW.
+// GLEW
+#define GLEW_STATIC
+#include <GL/glew.h>
+// GLFW
+#include <GLFW/glfw3.h>
+#include <iostream>
+
+int main()
+{
+    //Инициализация GLFW
+    glfwInit();
+    //Настройка GLFW
+    //Задается минимальная требуемая версия OpenGL.
+    //Мажорная
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    //Минорная
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    //Установка профайла для которого создается контекст
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    //Выключение возможности изменения размера окна
+    glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+
+    GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", nullptr, nullptr);
+    if (window == nullptr)
+    {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+
+    glewExperimental = GL_TRUE;
+    if (glewInit() != GLEW_OK)
+    {
+        std::cout << "Failed to initialize GLEW" << std::endl;
+        return -1;
+    }
+
+    int width, height;
+    glfwGetFramebufferSize(window, &width, &height);
+
+    glViewport(0, 0, width, height);
+
+    while(!glfwWindowShouldClose(window))
+    {
+        glfwPollEvents();
+        glfwSwapBuffers(window);
+    }
+
+    glfwTerminate();
+    return 0;
+}
